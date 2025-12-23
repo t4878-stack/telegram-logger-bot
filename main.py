1f2e144070135b771f751b94df0a0dcdd80675ce
t4878-stack <t4878440@gmail.com>
2025-12-23 14:46:07 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
-from datetime import datetime
+from datetime import datetime, timedelta
+import asyncio
 
-TOKEN = "BOT_TOKEN"
-
+TOKEN = "8542143557:AAEwuIFQCmyEU1EmiCEixA738H0UumiBt1I"
 
 # دیکشنری برای ذخیره پیام‌ها به ازای هر کاربر
 user_logs = {}
 
-# دستور /start
-
+def iran_now():
+    return datetime.utcnow() + timedelta(hours=3, minutes=30)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
@@ -17,22 +17,14 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "برای دیدن همه پیام‌ها /show رو بزن."
     )
 
-# ذخیره پیام‌ها با زمان
-
-
 async def log_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.message.from_user.id
     text = update.message.text
-    now = datetime.now().strftime("%H:%M")
-
+    now = iran_now().strftime("%H:%M")
     if user_id not in user_logs:
         user_logs[user_id] = []
-
     user_logs[user_id].append(f"ساعت {now} : {text}")
-    await update.message.reply_text(f" ساعت {now} : {text}")
-
-# نمایش همه پیام‌ها
-
+    await update.message.reply_text(f"ساعت {now} : {text}")
 
 async def show_logs(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.message.from_user.id
@@ -42,12 +34,24 @@ async def show_logs(update: Update, context: ContextTypes.DEFAULT_TYPE):
     else:
         await update.message.reply_text("هیچ پیامی ثبت نشده است.")
 
+async def daily_summary(app):
+    while True:
+        now = iran_now()
+        # ساعت 00:00 ایران
+        target = now.replace(hour=0, minute=0, second=0, microsecond=0)
+        if now >= target:
+            target += timedelta(days=1)
+        wait_seconds = (target - now).total_seconds()
+        await asyncio.sleep(wait_seconds)
+
+        # جمع‌بندی برای همه کاربران
+        for user_id, logs in user_logs.items():
+            if logs:
+                message = "💡 جمع‌بندی دیروز:\n" + "\n".join(logs)
+                await app.bot.send_message(chat_id=user_id, text=message)
+        # خالی کردن لیست‌ها
+        user_logs.clear()
+
 if __name__ == "__main__":
     app = ApplicationBuilder().token(TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("show", show_logs))
-    app.add_handler(MessageHandler(
-        filters.TEXT & ~filters.COMMAND, log_message))
-
-    print("Bot running...")
-    app.run_polling()
+    app.add_handler(CommandHandler("start",_
